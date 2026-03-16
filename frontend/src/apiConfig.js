de1633b59e5ac673fa873955f9226bf5981b624e
Suriya <suriya1252004@gmail.com>
2026-03-16 18:57:32 +0530

    update backend url

--- frontend/src/apiConfig.js
+++ frontend/src/apiConfig.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://seastarfishaquarium.onrender.com';
 
 export default API_BASE_URL;
