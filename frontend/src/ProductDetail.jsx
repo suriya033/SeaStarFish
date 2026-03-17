146b21471895dccda1ec506b7baacc332903b468
Suriya <suriya1252004@gmail.com>
2026-03-17 09:03:35 +0530

    deploy

--- frontend/src/ProductDetail.jsx
+++ frontend/src/ProductDetail.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { useParams, Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { useCart } from './CartContext';
 import API_BASE_URL from './apiConfig';
